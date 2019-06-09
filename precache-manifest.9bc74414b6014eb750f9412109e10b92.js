384ef62810b8b6b5a8ed9c67cecc5bac51f03f7a
Emilie Bialais <ebialais@gmail.com>
2019-06-09 11:09:17 +0200

    Updates

---
+++ precache-manifest.9bc74414b6014eb750f9412109e10b92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71641939ff3c5a039daf2e492d9b58e3",
+    "url": "/FamoTestAPI/index.html"
+  },
+  {
+    "revision": "f548a099a3106c415e50",
+    "url": "/FamoTestAPI/static/css/main.8034ea35.chunk.css"
+  },
+  {
+    "revision": "416b33bac9383e4eec90",
+    "url": "/FamoTestAPI/static/js/2.cf3fc958.chunk.js"
+  },
+  {
+    "revision": "f548a099a3106c415e50",
+    "url": "/FamoTestAPI/static/js/main.d83993e8.chunk.js"
+  },
+  {
+    "revision": "932e5a02b007674bee95",
+    "url": "/FamoTestAPI/static/js/runtime~main.5852a972.js"
+  }
+]);
